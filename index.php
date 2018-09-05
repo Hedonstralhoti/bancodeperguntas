d5e9c4414adfa7521ee2515ab8c779db659ff687
Hedonstralhoti <hedon.stralhoti1@gmail.com>
2018-09-05 19:44:32 -0300

    Signed-off-by: Hedonstralhoti <hedon.stralhoti1@gmail.com>

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<title>Banco de Perguntas</title>
+
+</head>
+<body>
+	<div class="container">
+		<!--Criando um nav bar que caso não caiba na pagina, tele terá um botão collapse-->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+			<!--Esse link na verdade cria uma area para o logo da empresa -->
+			<a href="#" class="navbar-brand">Gerador de Provas</a>
+			<!--esse é o botão que irá aparecer quando a nav bar não cabe na tela -->
+			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+				<!--Esse é o icone do botão -->
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<!--Aqui é a div do collapse -->
+			<div class="navbar-collapse collapse" id="navbarMenu">
+				
+				<div class="navbar-nav">
+				<a href="#" class="nav-item nav-link active">Home</a>
+				<a href="#" class="nav-item nav-link">Quem somos nós?</a>	
+				<a href="#" class="nav-item nav-link">Gerador de Provas</a>
+
+				</div>
+
+			</div>
+
+
+
+		</nav>
+	</div>
+
+<!-- -->
+
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
